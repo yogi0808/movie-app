cfddff3e503aec0cfffc2f822c377fc2f393a02b
yogi0808 <gojiyayogesh08@gmail.com>
2026-01-22 22:30:10 +0530

    Feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,38 @@
+import { FooterLinks } from "../constants"
+import Logo from "./Logo"
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="px-8 py-12 pb-20 text-white bg-primary flex gap-10 flex-wrap justify-center items-end">
+      <div className="flex flex-col gap-12">
+        <div className="w-32.5">
+          <Logo xl />
+        </div>
+        <button className="bg-white px-4 py-3 font-bold rounded text-[#01b4e4]">
+          Hi Yogesh!
+        </button>
+      </div>
+      <div className="flex gap-10 flex-wrap">
+        {FooterLinks.map((link) => (
+          <div
+            key={link.id}
+            className="flex flex-col"
+          >
+            <h2 className="uppercase font-extrabold mb-2">{link.title}</h2>
+            {link.links.map((link) => (
+              <a
+                key={link.id}
+                className="hover:underline font-semibold transition-colors ease-out duration-300"
+                href={link.link}
+              >
+                {link.text}
+              </a>
+            ))}
+          </div>
+        ))}
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/constants/index.js
+++ src/constants/index.js
@@ -36,3 +36,111 @@ export const HeaderLinks = [
     ],
   },
 ]
+
+export const FooterLinks = [
+  {
+    id: 1,
+    title: "The Basics",
+    links: [
+      {
+        id: 1,
+        text: "About TMDB",
+        link: "#",
+      },
+      {
+        id: 2,
+        text: "Contact Us",
+        link: "#",
+      },
+      {
+        id: 3,
+        text: "API Documentation",
+        link: "#",
+      },
+      {
+        id: 4,
+        text: "API for Business",
+        link: "#",
+      },
+      {
+        id: 5,
+        text: "System Status",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Get Involved",
+    links: [
+      {
+        id: 1,
+        text: "Contribution Bible",
+        link: "#",
+      },
+      {
+        id: 2,
+        text: "Add New Movie",
+        link: "#",
+      },
+      {
+        id: 3,
+        text: "Add New TV Show",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Community",
+    links: [
+      {
+        id: 1,
+        text: "Guidelines",
+        link: "#",
+      },
+      {
+        id: 2,
+        text: "Discussions",
+        link: "#",
+      },
+      {
+        id: 3,
+        text: "Leaderboard",
+        link: "#",
+      },
+      {
+        id: 4,
+        text: "Support Forums",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Legal",
+    links: [
+      {
+        id: 1,
+        text: "Terms of Use",
+        link: "#",
+      },
+      {
+        id: 2,
+        text: "API Terms of Use",
+        link: "#",
+      },
+      {
+        id: 3,
+        text: "Privacy Policy",
+        link: "#",
+      },
+      {
+        id: 4,
+        text: "DMCA Policy",
+        link: "#",
+      },
+    ],
+  },
+
+]
