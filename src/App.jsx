2ba7d11d3c9ce7eaf0bfc0e04da6561bc0660781
yogi0808 <gojiyayogesh08@gmail.com>
2026-01-22 13:32:04 +0530

    Feat: RootLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router"
+<<<<<<< Updated upstream
+=======
+import HomeScreen from "./screens/HomeScreen"
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -6,7 +10,11 @@ function App() {
       <Routes>
         <Route
           path="/"
+<<<<<<< Updated upstream
           element={<h1>Hello</h1>}
+=======
+          element={<HomeScreen />}
+>>>>>>> Stashed changes
         />
       </Routes>
     </BrowserRouter>
