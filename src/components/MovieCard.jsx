3876c50bf42bb6b667da0eaeced3593e7c9fe8ee
yogi0808 <gojiyayogesh08@gmail.com>
2026-01-27 00:44:28 +0530

    Feat: movie card created

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,18 @@
+const MovieCard = ({ src, name, date }) => {
+  return (
+    <div className="max-w-37.5 cursor-pointer flex flex-col content-stretch">
+      <div className="w-37.5 bg-stone-200/60 rounded-lg">
+        <img
+          src={src}
+          className="rounded-lg w-full"
+        />
+      </div>
+      <div className="px-2 pt-6">
+        <a className="font-bold leading-6">{name}</a>
+        <p className="text-stone-500 leading-6">{date}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
