3589769ba91fde8d07d35b9cb92901b77cd6c955
yogi0808 <gojiyayogesh08@gmail.com>
2026-02-05 17:14:05 +0530

    Feat: added box commponent with divider

---
+++ src/components/BoxWithDivider.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const BoxWithDivider = ({ children }) => {
+  return (
+    <div className="p-4 pt-3.5 border-t border-search-border">{children}</div>
+  )
+}
+
+export default BoxWithDivider
--- src/screens/MoviesScreen.jsx
+++ src/screens/MoviesScreen.jsx
@@ -1,3 +1,4 @@
+import BoxWithDivider from "@/components/BoxWithDivider"
 import CollapsibleCard from "@/components/filters/CollapsibleCard"
 import RootLayout from "@/layouts/RootLayout"
 import React from "react"
@@ -9,7 +10,7 @@ const MoviesScreen = () => {
         <h2 className="font-semibold text-2xl mb-5">Popular Movies</h2>
         <div className="md:w-65">
           <CollapsibleCard>
-            <p>Hello</p>
+            <BoxWithDivider>Box with Divider</BoxWithDivider>
           </CollapsibleCard>
         </div>
         <div></div>
