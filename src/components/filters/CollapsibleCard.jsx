74bb2bcb92199dd33a201d172d41c71b85ff949f
yogi0808 <gojiyayogesh08@gmail.com>
2026-02-05 17:15:11 +0530

    Fix: collapsible card

--- src/components/filters/CollapsibleCard.jsx
+++ src/components/filters/CollapsibleCard.jsx
@@ -1,15 +1,15 @@
 import React, { Activity, useState } from "react"
 import { MdKeyboardArrowRight } from "react-icons/md"
 
-const CollapsibleCard = ({ children }) => {
+const CollapsibleCard = ({ title, children }) => {
   const [isActive, setIsActive] = useState(false)
   return (
     <div className="max-w-65 shadow-card border border-search-border rounded-lg flex flex-col">
       <div
-        className="flex justify-between px-4 py-3.5"
+        className="flex justify-between px-4 py-3.5 cursor-pointer"
         onClick={() => setIsActive((prev) => !prev)}
       >
-        <h2 className="text-lg font-semibold">Sort</h2>
+        <h2 className="text-lg font-semibold">{title}</h2>
         <MdKeyboardArrowRight className="text-xl origin-center" />
       </div>
       <Activity mode={isActive ? "visible" : "hidden"}>{children}</Activity>
--- src/screens/MoviesScreen.jsx
+++ src/screens/MoviesScreen.jsx
@@ -9,7 +9,7 @@ const MoviesScreen = () => {
       <section className="max-w-325 mx-auto px-5 md:px-10 py-4 md:py-7.5">
         <h2 className="font-semibold text-2xl mb-5">Popular Movies</h2>
         <div className="md:w-65">
-          <CollapsibleCard>
+          <CollapsibleCard title="Sort">
             <BoxWithDivider>Box with Divider</BoxWithDivider>
           </CollapsibleCard>
         </div>
