30c2dc248c8bf5248510f49892227c2ce7144559
yogi0808 <gojiyayogesh08@gmail.com>
2026-02-06 06:18:03 +0530

    Fix: box with divider added classnames as params

--- src/components/BoxWithDivider.jsx
+++ src/components/BoxWithDivider.jsx
@@ -1,9 +1,15 @@
+import classNames from "classnames"
 import React from "react"
 
-const BoxWithDivider = ({ children }) => {
-  return (
-    <div className="p-4 pt-3.5 border-t border-search-border">{children}</div>
+const BoxWithDivider = ({ children, className }) => {
+  const mainClassNames = classNames(
+    "p-4 pt-3.5 border-t border-search-border",
+    {
+      [className]: className,
+    },
   )
+
+  return <div className={mainClassNames}>{children}</div>
 }
 
 export default BoxWithDivider
--- src/sections/movies/FilterSection.jsx
+++ src/sections/movies/FilterSection.jsx
@@ -1,12 +1,13 @@
 import BoxWithDivider from "@/components/BoxWithDivider"
 import DropDown from "@/components/DropDown"
 import CollapsibleCard from "@/components/filters/CollapsibleCard"
-import { countriesOptions, sortOptions } from "@/constants"
+import { sortOptions } from "@/constants"
+import countries from "@constants/countries.json"
 import React, { useState } from "react"
 
 const FilterSection = () => {
   const [sort, setSort] = useState(sortOptions[0])
-  const [country, setCountry] = useState(countriesOptions[101])
+  const [country, setCountry] = useState(countries[101])
 
   return (
     <div className="md:w-65 flex flex-col gap-3">
@@ -26,7 +27,7 @@ const FilterSection = () => {
             label="Country"
             selected={country}
             handleSelect={setCountry}
-            list={countriesOptions}
+            list={countries}
             search
           />
         </BoxWithDivider>
