98d35bdef4fbf3b6faeb103827d375a76438922d
yogi0808 <gojiyayogesh08@gmail.com>
2026-02-08 17:22:55 +0530

    Fix: dummy options list

--- src/components/filters/DummyOptions.jsx
+++ src/components/filters/DummyOptions.jsx
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import BoxWithDivider from "../BoxWithDivider"
 
 const DummyOptions = () => {
+  const [isChecked, setIsChecked] = useState(true)
+
   return (
     <>
       <BoxWithDivider>
@@ -43,10 +45,57 @@ const DummyOptions = () => {
           <input
             type="checkbox"
             className="checkbox"
+            onChange={() => setIsChecked((prev) => !prev)}
             defaultChecked
           />
           <span>Search all availabilities?</span>
         </label>
+        {isChecked ? (
+          ""
+        ) : (
+          <div className="mt-1.5">
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox"
+                defaultChecked
+              />
+              <span>Steam</span>
+            </label>
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox"
+                defaultChecked
+              />
+              <span>Free</span>
+            </label>
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox"
+                defaultChecked
+              />
+              <span>Ads</span>
+            </label>
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox"
+                defaultChecked
+              />
+              <span>Rent</span>
+            </label>
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox"
+                defaultChecked
+              />
+              <span>Buy</span>
+            </label>
+          </div>
+        )}
       </BoxWithDivider>
     </>
   )
