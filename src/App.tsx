8c43e9a4fb7e421b07fffc9cdf1acf6c39e36012
yogi0808 <gojiyayogesh08@gmail.com>
2026-03-06 12:29:20 +0530

    Feat: added vercel file to tell vercel that this is spa

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import MoviesScreen from '@screens/MoviesScreen';
 import ReviewScreen from '@screens/ReviewScreen';
 import DetailsScreen from '@screens/DetailsScreen';
 import RegisterScreen from '@screens/RegisterScreen';
+import NotFoundScreen from '@screens/NotFoundScreen';
 import ResetPasswordScreen from '@screens/ResetPasswordScreen';
 import ForgotPasswordScreen from '@screens/ForgotPasswordScreen';
 import ResendVerificationScreen from '@screens/ResendVerificationScreen';
-import NotFoundScreen from '@screens/NotFoundScreen';
 
 /**
  * this is a root component of the application hear i created and and setup the routers
@@ -19,23 +19,21 @@ import NotFoundScreen from '@screens/NotFoundScreen';
  */
 const App: React.FC = () => {
   return (
-    <div className="bg-wrapper">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
-          <Route path="/resend-verification" element={<ResendVerificationScreen />} />
-          <Route path="/reset-password" element={<ResetPasswordScreen />} />
-          <Route path="/movies" element={<MoviesScreen />} />
-          <Route path="/details/:id" element={<DetailsScreen />} />
-          <Route path="/review/:reviewId" element={<ReviewScreen />} />
-          <Route path="/tv" element={<TvScreen />} />
-          <Route path="*" element={<NotFoundScreen />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+        <Route path="/resend-verification" element={<ResendVerificationScreen />} />
+        <Route path="/reset-password" element={<ResetPasswordScreen />} />
+        <Route path="/movies" element={<MoviesScreen />} />
+        <Route path="/details/:id" element={<DetailsScreen />} />
+        <Route path="/review/:reviewId" element={<ReviewScreen />} />
+        <Route path="/tv" element={<TvScreen />} />
+        <Route path="*" element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
